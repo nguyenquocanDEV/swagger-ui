48c5d3364663032b6d4a053417cb9d8b6c7e130d
nguyenquocanDEV <nguyenquocan2001789@gmail.com>
2021-09-10 01:46:30 -0700

    T001: Login Screens

--- src/main/resources/login.sql
+++ src/main/resources/login.sql
@@ -1,11 +1,19 @@
-CREATE TABLE login.user (
-     id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-     username CHAR(30) NOT NULL,
-     password CHAR(30) NOT NULL
-     
+CREATE
+database  login;
+use
+login;
+
+CREATE TABLE login.user
+(
+
+    username CHAR(30) NOT NULL PRIMARY KEY,
+    password CHAR(30) NOT NULL
+
 );
 
-INSERT INTO login.user (username,password) VALUES
-    ('admin','admin'), ('quocan','123');
+INSERT INTO login.user (username, password)
+VALUES ('admin', 'admin'),
+       ('quocan', '123');
 
-SELECT * FROM login.user;
+SELECT *
+FROM login.user;
